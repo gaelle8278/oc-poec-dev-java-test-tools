a297709ba68124ae0a89d4edeff417ef2b74b2de
gaelle <gaelle.rauffet@kiwiscan.net>
2019-09-27 13:59:48 +0200

    initialisation log4j

--- src/main/java/dev/gaelle_rauffet/test_java_tools/Main.java
+++ src/main/java/dev/gaelle_rauffet/test_java_tools/Main.java
@@ -1,9 +1,14 @@
 package dev.gaelle_rauffet.test_java_tools;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
 
+	private static final Logger logger = LogManager.getLogger(Main.class);
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		logger.info("Démarrage de l'application");
 
 	}
 
